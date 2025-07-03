ddc857f72efb0eb5c0bac611cfce70c52aa46ed0
Alekyanarmeta <alekyanarmeta9@gmail.com>
2025-07-03 12:34:59 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,10 @@ import Profile from "../components/profile";
 import Favourites from "../components/favourites";
 import Orders from "../components/orders";
 import Settings from "../components/setting";
+import AddBook from "../components/updateBook";
 function App() {
   const dispatch = useDispatch()
+  const role=localStorage.getItem("role")
   useEffect(
     () => {
       if (localStorage.getItem("id") && localStorage.getItem("token") && localStorage.getItem("role")) {
@@ -25,21 +27,36 @@ function App() {
     }, []
   )
   return (
-    <div>
+    <div className="min-vw-100 min-vh-100 d-flex flex-column justify-content-between" style={{ backgroundColor: "#0F52BA", }}>
+      <div className="bg-light">
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/all-books" element={<Allbooks />}></Route>
         <Route exact path="/cart" element={<Cart />}></Route>
         <Route exact path="/profile" element={<Profile />}>
-          <Route index path="favourites" element={<Favourites />} />
+        {
+          role=="user" ?(
+          <>
+          <Route index  element={<Favourites />} />
+          <Route  path="favourites" element={<Favourites />} />
           <Route path="orders" element={<Orders />} />
           <Route path="settings" element={<Settings />} />
+          </>):(
+            <>
+            <Route index element={<Orders />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="addbook" element={<AddBook/>}/>
+            </>
+          )
+        }
+
         </Route>
         <Route exact path="/login" element={<Login />}></Route>
         <Route exact path="/signin" element={<Signin />}></Route>
         <Route exact path="/view-books/:id" element={<Bookdetails />}></Route>
       </Routes>
+      </div>
       <Footer />
     </div>
   )

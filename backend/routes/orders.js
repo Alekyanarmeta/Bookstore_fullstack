9f2dfcee1ece4ddc700a6100a79504dd7d14c9a3
Alekyanarmeta <alekyanarmeta9@gmail.com>
2025-07-03 13:09:37 +0530

    Update orders.js

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { default: axios } = require("axios");
 const Books = require("../models/Books");
 const User = require("../models/User");
 const Order = require("../models/order");
